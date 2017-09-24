26ffd1e771b87593cdda6f2de73b700fe1f690d7
Neha Goel <nehagv2325@gmail.com>
2017-09-24 13:46:27 -0500

    titanic_adaline updated

--- Assignment_2/Titanic_Adaline.py
+++ Assignment_2/Titanic_Adaline.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 titanic = pd.read_csv("/Users/nehansh/Documents/MachineLearning_Class/Assignment_2/train.csv")
 titanic_test = pd.read_csv("/Users/nehansh/Documents/MachineLearning_Class/Assignment_2/test.csv")
 #cleansing
@@ -12,4 +14,97 @@ for df in [titanic, titanic_test]:
     #     df.loc[df["Embarked"]==port, "Embarked"] = i
     for col in ["Age", "Fare"]:
         df[col] = df[col].fillna(titanic[col].median())
-    print(df.describe())
+    # print(df.describe())
+titanic.to_csv('train_preprocess.csv')
+titanic_test.to_csv('test_preprocess.csv')
+
+
+class AdalineGD(object):
+    """ADAptive LInear NEuron classifier.
+
+    Parameters
+    ------------
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    n_iter : int
+        Passes over the training dataset.
+
+    Attributes
+    -----------
+    w_ : 1d-array
+        Weights after fitting.
+    cost_ : list
+        Sum-of-squares cost function value in each epoch.
+
+    """
+    def __init__(self, eta=0.01, n_iter=50):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples and
+            n_features is the number of features.
+        y : array-like, shape = [n_samples]
+            Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            # Please note that the "activation" method has no effect
+            # in the code since it is simply an identity function. We
+            # could write `output = self.net_input(X)` directly instead.
+            # The purpose of the activation is more conceptual, i.e.,
+            # in the case of logistic regression, we could change it to
+            # a sigmoid function to implement a logistic regression classifier.
+            output = self.activation(X)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return self.net_input(X)
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+
+# select age and sex
+y = df.iloc[0:900, 4].values
+y = np.where(y == '0', -1, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [4, 5]].values
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+
+ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+
+plt.tight_layout()
+# plt.savefig('./adaline_1.png', dpi=300)
+plt.show()
