85efb337acaa5026ef641a2e59a4af37448e613a
Neha Goel <nehagv2325@gmail.com>
2017-09-24 12:51:40 -0500

    update

---
+++ Assignment_2/Titanic_Adaline.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+titanic = pd.read_csv("/Users/nehansh/Documents/MachineLearning_Class/Assignment_2/train.csv")
+titanic_test = pd.read_csv("/Users/nehansh/Documents/MachineLearning_Class/Assignment_2/test.csv")
+#cleansing
+for df in [titanic, titanic_test]:
+    df.loc[df["Sex"] == "male", "Sex"] = 0
+    df.loc[df["Sex"] == "female", "Sex"] = 1
+    df["Embarked"] = df["Embarked"].fillna("S")
+    ports = list(df["Embarked"].unique())
+    for i,port in enumerate(ports):
+        df.loc[df["Embarked"]==port, "Embarked"] = i
+    for col in ["Age", "Fare"]:
+        df[col] = df[col].fillna(titanic[col].median())
+    print(df.describe())
